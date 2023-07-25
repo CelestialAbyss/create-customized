8b7edf7cef3dce023b788aa7512f8d3e3a21f9a4
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2023-07-24 23:01:11 -0400

    Added additional files...

--- kubejs_future/server_scripts/minecraft.js
+++ kubejs_future/server_scripts/minecraft.js
@@ -1,4 +1,4 @@
-// everything here
+// everything here must be vanilla create and minecraft for easy drag and drop.
 
 ServerEvents.recipes(event => {
     event.remove({id: 'minecraft:redstone_from_smelting_redstone_ore'}) // cursed recipe
